6782a1d790640f7c0f6c4f131d66e2c3c293be3b
Kauê Corrêa <kauehenrique08@gmail.com>
2023-06-27 10:17:48 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,16 @@
+speed = 240
+print(type(speed))
+
+welcome = 'boa noite dev'
+print(welcome)
+
+posso = False #boolean
+dollar = 4.70
+print(type(posso))
+print(type(welcome))
+print(type(speed))
+
+velocidade, frase, posso_ou_nao = 100, 'Olá', True
+print(velocidade)
+print(frase)
+print(posso_ou_nao)
